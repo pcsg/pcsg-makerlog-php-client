3c658c5a0457afb9e91818e7e64e2c767c66575a
Henning Leutz <leutz@pcsg.de>
2019-01-22 20:32:53 +0100

    refactor: change main Makerlog Class -> add oAuth Authorization bearer; add getOptions Methods; add debug flag for request

--- src/PCSG/Makerlog/Makerlog.php
+++ src/PCSG/Makerlog/Makerlog.php
@@ -19,14 +19,24 @@ use GuzzleHttp;
 class Makerlog
 {
     /**
-     * @var null|Api\Users
+     * @var Api\Users
      */
     protected $Users = null;
 
     /**
-     * @var null|Api\Projects
+     * @var Api\Notifications
+     */
+    protected $Notifications = null;
+
+    /**
+     * @var Api\Projects
      */
     protected $Projects = null;
+
+    /**
+     * @var Api\Tasks
+     */
+    protected $Tasks = null;
 
     /**
      * @var array
@@ -42,20 +52,40 @@ class Makerlog
      * - client_id
      * - client_secret
      * - api_endpoint
+     * - access_token
+     * - refresh_token
+     * - debug
      */
     public function __construct($options = [])
     {
         $allowed = [
             'client_id',
             'client_secret',
-            'api_endpoint'
+            'api_endpoint',
+            'access_token',
+            'refresh_token',
+            'debug'
         ];
+
+        // defaults
+        $this->options['debug'] = false;
 
         foreach ($allowed as $item) {
             if (isset($options[$item])) {
                 $this->options[$item] = $options[$item];
             }
         }
+    }
+
+    /**
+     * Return a wanted option
+     *
+     * @param string $option
+     * @return mixed
+     */
+    public function getOptions($option)
+    {
+        return $this->options[$option];
     }
 
     //region api getters
@@ -89,19 +119,31 @@ class Makerlog
     }
 
     /**
-     * @todo
+     * Returns the tasks api object
+     *
+     * @return Api\Tasks
      */
     public function getTasks()
     {
+        if ($this->Tasks === null) {
+            $this->Tasks = new Api\Tasks($this);
+        }
 
+        return $this->Tasks;
     }
 
     /**
-     * @todo
+     * Returns the notifications api object
+     *
+     * @return Api\Notifications
      */
     public function getNotifications()
     {
+        if ($this->Notifications === null) {
+            $this->Notifications = new Api\Notifications($this);
+        }
 
+        return $this->Notifications;
     }
 
     /**
@@ -125,15 +167,26 @@ class Makerlog
     public function getRequest()
     {
         $apiEndpoint = 'https://api.getmakerlog.com';
-        $Guzzle      = new GuzzleHttp\Client();
 
         if (!empty($this->options['api_endpoint'])) {
             $apiEndpoint = $this->options['api_endpoint'];
         }
 
-        // @todo oauth
+        if (!empty($this->options['access_token'])) {
+            $Client = new GuzzleHttp\Client([
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->options['access_token'],
+                    'Content-Type' => 'application/json'
+                ],
+                'debug' => $this->options['debug']
+            ]);
+        } else {
+            $Client = new GuzzleHttp\Client([
+                'debug' => $this->options['debug']
+            ]);
+        }
 
-        return new Request($apiEndpoint, $Guzzle);
+        return new Request($apiEndpoint, $Client);
     }
 
     //endregion
