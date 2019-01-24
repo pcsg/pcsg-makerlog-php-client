fb78321342d10e7ec97b055d49d44e804b569ce8
Henning Leutz <leutz@pcsg.de>
2019-01-24 19:45:03 +0100

    feat: getUnreadCount() markRead() markAllRead()

--- src/PCSG/Makerlog/Api/Notifications.php
+++ src/PCSG/Makerlog/Api/Notifications.php
@@ -33,12 +33,62 @@ class Notifications
     }
 
     /**
-     * @return mixed
+     * @return array
      * @throws Exception
      */
     public function get()
     {
         $Request = $this->Makerlog->getRequest()->get('/notifications');
+        $projects = json_decode($Request->getBody());
+
+        return $projects;
+    }
+
+    /**
+     * Return the number of unread notifications
+     * This can use for polling
+     *
+     * @return integer
+     * @throws Exception
+     */
+    public function getUnreadCount()
+    {
+        $Request = $this->Makerlog->getRequest()->get('/notifications/unread_count');
+        $projects = json_decode($Request->getBody());
+
+        return (int)$projects->unread_count;
+    }
+
+    /**
+     * Mark a notification as read
+     *
+     * @param integer $notificationId
+     * @return mixed
+     * @throws Exception
+     *
+     * @todo currently not tested
+     */
+    public function markRead($notificationId)
+    {
+        $notificationId = (int)$notificationId;
+
+        $Request = $this->Makerlog->getRequest()->post('/notifications/' . $notificationId . '/mark_read');
+        $projects = json_decode($Request->getBody());
+
+        return $projects;
+    }
+
+    /**
+     * Mark all notifications as read
+     *
+     * @return mixed
+     * @throws Exception
+     *
+     * @todo currently not tested
+     */
+    public function markAllRead()
+    {
+        $Request = $this->Makerlog->getRequest()->post('/notifications/mark_all_read');
         $projects = json_decode($Request->getBody());
 
         return $projects;
