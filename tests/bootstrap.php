dcdd74cca4170637af0982623f81872632af25e9
Henning Leutz <leutz@pcsg.de>
2019-01-26 10:33:24 +0100

    refactor: ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__, 2).'/vendor/autoload.php';
+require_once dirname(__FILE__, 2) . '/vendor/autoload.php';
 
 //  ../vendor/bin/phpunit
 //  ../vendor/bin/phpunit --coverage-clover=./reports/clover.xml --coverage-html=./reports
@@ -18,7 +18,16 @@ class MakerLogTest
      */
     public static function getMakerlog()
     {
-        $config   = require dirname(__FILE__).'/config.php';
+        if (file_exists(dirname(__FILE__) . '/config.php')) {
+            $config = require dirname(__FILE__) . '/config.php';
+        } else {
+            $config = [
+                'client_id' => getenv('client_id'),
+                'client_secret' => getenv('client_secret'),
+                'access_token' => getenv('access_token')
+            ];
+        }
+
         $Makerlog = new PCSG\Makerlog\Makerlog($config);
 
         return $Makerlog;
