61b94fbbb8585e0744ae931b667cfd6a083e0d8f
Henning Leutz <leutz@pcsg.de>
2019-01-26 10:45:07 +0100

    fix: unstable tests commented out for now

--- tests/PCSG/Makerlog/Api/ProjectsTest.php
+++ tests/PCSG/Makerlog/Api/ProjectsTest.php
@@ -6,10 +6,23 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * Projects Tests
+ *
+ * @todo implement
+ * api is currently not stable at the moment
  */
+/*
 class ProjectsTest extends TestCase
 {
     //region getter
 
+    public function testGetList()
+    {
+        $Makerlog = \MakerLogTest::getMakerlog();
+        $projects = $Makerlog->getProjects()->getList();
+
+        $this->assertNotEmpty($projects);
+    }
+
     //endregion
 }
+*/
--- tests/PCSG/Makerlog/Api/TasksTest.php
+++ tests/PCSG/Makerlog/Api/TasksTest.php
@@ -6,10 +6,22 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * Tasks Tests
+ *
+ * @todo task api is not stable at the moment - lot of timeouts
  */
+/*
 class TasksTest extends TestCase
 {
     //region getter
 
+    public function testGetList()
+    {
+        $Makerlog = \MakerLogTest::getMakerlog();
+        $tasks = $Makerlog->getTasks()->getList();
+
+        $this->assertNotEmpty($tasks);
+    }
+
     //endregion
 }
+*/
