690e2afcb769dbd7cd164ad7682066be6d7871b9
Henning Leutz <leutz@pcsg.de>
2019-01-26 15:25:15 +0100

    tests: added testGetRequestApiPoint

--- tests/PCSG/Makerlog/Api/MakerlogTest.php
+++ tests/PCSG/Makerlog/Api/MakerlogTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\PCSG\Makerlog\Api;
 
+use PCSG\Makerlog\Makerlog;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -21,5 +22,21 @@ class MakerlogTest extends TestCase
         $this->assertIsString(
             \MakerLogTest::getMakerlog()->getOption('client_id')
         );
+    }
+
+    public function testGetRequestApiPoint()
+    {
+        $Default = \MakerLogTest::getMakerlog();
+
+        $Makerlog = new Makerlog([
+            'client_id'     => $Default->getOption('client_id'),
+            'client_secret' => $Default->getOption('client_secret'),
+            'access_token'  => $Default->getOption('access_token'),
+            'api_endpoint'  => 'https://api.getmakerlog.com'
+        ]);
+
+        $DeHenne = $Makerlog->getUsers()->getByUsername('dehenne');
+
+        $this->assertIsString($DeHenne->username);
     }
 }
