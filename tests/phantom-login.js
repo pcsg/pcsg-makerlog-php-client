14ca8f88f02b98f0c6ed9b4bb8d19ea2e8f9cebc
Henning Leutz <leutz@pcsg.de>
2019-01-29 08:01:51 +0100

    tests: phantomjs script to get new oauth tokens

---
+++ tests/phantom-login.js
@@ -0,0 +1,153 @@
+/**
+ * This script fetches new oauth tokens via phantomjs
+ *
+ * @author www.pcsg.de (Henning Leutz - https://twitter.com/de_henne)
+ */
+
+let debug          = false;
+let testindex      = 0;
+let loadInProgress = false;
+
+let username = process.env.MAKERLOG_USERNAME;
+let password = process.env.MAKERLOG_PASSWORD;
+
+/********** SETTINGS *********************/
+
+let webPage = require('webpage');
+let page    = webPage.create();
+
+page.settings.javascriptEnabled = true;
+page.settings.loadImages        = false; // is faster
+
+phantom.cookiesEnabled    = true;
+phantom.javascriptEnabled = true;
+
+/********** SETTINGS END *****************/
+
+let logger = function (msg) {
+    if (debug) {
+        console.log(msg);
+    }
+};
+
+page.onConsoleMessage = function (msg) {
+    if (debug) {
+        console.log(msg);
+    }
+};
+
+function clickHelper(boundingClientRect) {
+    page.sendEvent(
+        'click',
+        boundingClientRect.left + boundingClientRect.width / 2,
+        boundingClientRect.top + boundingClientRect.height / 2
+    );
+}
+
+/********** DEFINE STEPS ***********************/
+
+let steps = [
+
+    function () {
+        logger('');
+        logger('Step 1 - Open oauth page');
+
+        page.open("http://henbug.seaofsin.de/makerlist/", function (status) {
+
+        });
+    },
+
+    function () {
+        logger('');
+        logger('Step 2 - Login');
+
+        let submit = page.evaluate(function (username, password) {
+            var LoginForm = document.querySelector('form[action="/api-auth/login/"]');
+
+            if (LoginForm) {
+                document.querySelector('[name="username"]').value = username;
+                document.querySelector('[name="password"]').value = password;
+
+                return document.querySelector('[type="submit"]').getBoundingClientRect();
+            }
+
+            return null;
+        }, username, password);
+
+        if (submit) {
+            logger('- submit');
+            loadInProgress = true;
+            clickHelper(submit);
+        }
+    },
+
+    function () {
+        logger('');
+        logger('Step 3 - Accept permissions');
+
+        page.evaluate(function () {
+            let a = document.querySelector('form[id="authorizationForm"] [name="allow"]');
+            let e = document.createEvent('MouseEvents');
+
+            e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            a.dispatchEvent(e);
+        });
+
+        logger('- submit');
+        loadInProgress = true;
+        //clickHelper(submit);
+    },
+
+    function () {
+        logger('');
+        logger('Step 4 - Read oauth tokens');
+
+        let result = page.evaluate(function () {
+            return document.body.innerHTML.replace('<pre>', '').replace('</pre>', '');
+        });
+
+        console.log(result);
+    }
+];
+
+/********** END STEPS ***********************/
+
+logger('All settings loaded, start with execution');
+
+let interval = setInterval(executeRequestsStepByStep, 50);
+
+function executeRequestsStepByStep() {
+    if (loadInProgress === false && typeof steps[testindex] == "function") {
+        steps[testindex]();
+        testindex++;
+    }
+
+    if (typeof steps[testindex] != "function") {
+        clearInterval(interval);
+        exit();
+    }
+}
+
+function exit() {
+    page.stop();
+    page.close();
+    phantom.exit(0);
+}
+
+/**
+ * listener
+ */
+
+page.onLoadStarted = function () {
+    loadInProgress = true;
+    logger('Loading started');
+};
+
+page.onLoadFinished = function () {
+    loadInProgress = false;
+    logger('Loading finished');
+};
+
+page.onConsoleMessage = function (msg) {
+    logger(msg);
+};
