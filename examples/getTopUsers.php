a09fe1a1bc7ced2c9a52e0032a1edfd96e437f48
Henning Leutz <leutz@pcsg.de>
2019-02-01 07:17:17 +0100

    feat: refactor: moved examples stuff into docs; added real examples to the examples folder

---
+++ examples/getTopUsers.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This script shows how to get the Maker Of the Day
+ *
+ * @author www.pcsg.de (Henning Leutz - https://twitter.com/de_henne)
+ */
+
+require_once dirname(__FILE__, 2).'/vendor/autoload.php';
+
+$Makerlog = new PCSG\Makerlog\Makerlog();
+
+$Stats = $Makerlog->getStats();
+$data  = $Stats->getStats();
+
+$newUsers = $data->top_users;
+
+foreach ($newUsers as $user) {
+    echo $user->username.PHP_EOL;
+}
+
+echo PHP_EOL;
