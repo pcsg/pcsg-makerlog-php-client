ddc5919a3618768722a674cf00caf54bd50db000
Henning Leutz <leutz@pcsg.de>
2019-03-12 07:23:40 +0100

    feat: Projects API are implemented

--- src/PCSG/Makerlog/Api/Projects/Project.php
+++ src/PCSG/Makerlog/Api/Projects/Project.php
@@ -12,6 +12,9 @@ use PCSG\Makerlog\Makerlog;
 /**
  * Class Project
  *
+ * - a project is similar to a hashtag
+ * - a project is part of a product
+ *
  * @package PCSG\Makerlog\Api\Products
  */
 class Project
@@ -91,7 +94,7 @@ class Project
      */
     public function delete()
     {
-        $this->Makerlog->getRequest()->delete('/projects/' . $this->projectId . '/');
+        $this->Makerlog->getRequest()->delete('/projects/'.$this->projectId.'/');
     }
 
     //endregion
@@ -111,12 +114,118 @@ class Project
     /**
      * Return the project name
      *
-     * @return mixed
+     * @return string
      * @throws Exception
      */
     public function getName()
     {
         return $this->getProjectData()->name;
+    }
+
+    /**
+     * Gets user and products associated to this project / hashtag.
+     *
+     * @throws Exception
+     */
+    public function getRelated()
+    {
+        $this->Makerlog->getRequest()->get('/projects/'.$this->projectId.'/related/');
+    }
+
+    /**
+     * Is the project private?
+     *
+     * @return bool
+     */
+    public function isPrivate()
+    {
+        try {
+            return (bool)$this->getProjectData()->private;
+        } catch (Exception $Exception) {
+            return true;
+        }
+    }
+
+    /**
+     * Is the project public?
+     *
+     * @return bool
+     */
+    public function isPublic()
+    {
+        try {
+            return !(bool)$this->getProjectData()->private;
+        } catch (Exception $Exception) {
+            return false;
+        }
+    }
+
+    //endregion
+
+    //region change, setter, update
+
+    /**
+     * Update / change the task
+     *
+     * @param array $options - optional, default = [
+     *      "name"     => '',
+     *      "private"  => false,
+     *      "user"     => true
+     * ]
+     *
+     * @throws Exception
+     */
+    public function update($options = [])
+    {
+        if (empty($options)) {
+            throw new Exception("Options can't be empty", 400);
+        }
+
+        $params  = [];
+        $allowed = ['name', 'private', 'user'];
+
+        foreach ($allowed as $key) {
+            if (isset($options[$key])) {
+                $params[$key] = $options[$key];
+            }
+        }
+
+        if (empty($params)) {
+            throw new Exception(
+                "Can't send the update request. Data are empty. Options has forbidden entries",
+                406
+            );
+        }
+
+        $this->Makerlog->getRequest()->patch('/projects/'.$this->projectId.'/', [
+            'form_params' => $params
+        ]);
+
+        $this->refresh();
+    }
+
+    /**
+     * Changes the project status to public
+     *
+     * @throws Exception
+     */
+    public function setToPublic()
+    {
+        $this->update([
+            'private' => false
+        ]);
+    }
+
+    /**
+     * Changes the project status to private
+     *
+     * @throws Exception
+     */
+    public function setToPrivate()
+    {
+        $this->update([
+            'private' => true
+        ]);
     }
 
     //endregion
